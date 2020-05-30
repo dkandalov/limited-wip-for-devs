aaff288ba8316bef81c37cf19b29cb1e54ddd7db
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-05-30 10:49:10 +0100

    ...

--- src/test/kotlin/FizzBuzzWoofTests.kt
+++ src/test/kotlin/FizzBuzzWoofTests.kt
@@ -5,9 +5,11 @@ class FizzBuzzWoofTests {
     @Test fun `convert number to FizzBuzzWoof string`() {
         fizzBuzzWoof(1) shouldEqual "1"
         fizzBuzzWoof(2) shouldEqual "2"
+        fizzBuzzWoof(3) shouldEqual "Fizz"
     }
 }
 
 fun fizzBuzzWoof(n: Int): String {
+    if (n.rem(3) == 0) return "Fizz"
     return n.toString()
 }
