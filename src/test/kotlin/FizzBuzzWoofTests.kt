4416b776c3907ef9b152374ba61dd85e8281eb69
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-05-30 10:21:50 +0100

    test

--- src/test/kotlin/FizzBuzzWoofTests.kt
+++ src/test/kotlin/FizzBuzzWoofTests.kt
@@ -1,7 +1,8 @@
+import kotlincommon.test.shouldEqual
 import org.junit.jupiter.api.Test
 
 class FizzBuzzWoofTests {
     @Test fun `convert number to FizzBuzzWoof string`() {
-
+        1 shouldEqual 1
     }
 }
