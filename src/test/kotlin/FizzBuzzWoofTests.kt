06e38361633418ae8d0fd364b7452cdfca91192e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-05-30 10:36:34 +0100

    refactoring

--- src/test/kotlin/FizzBuzzWoofTests.kt
+++ src/test/kotlin/FizzBuzzWoofTests.kt
@@ -20,13 +20,15 @@ class FizzBuzzWoofTests {
     }
 }
 
+fun fizz(n: Int) = if (n.rem(3) == 0) "Fizz" else ""
+
 fun fizzBuzzWoof(n: Int): String {
-    return when {
-        n.rem(3) == 0 && n.rem(5) == 0 && n.rem(7) == 0 -> "FizzBuzzWoof"
-        n.rem(3) == 0 && n.rem(7) == 0                  -> "FizzWoof"
+    return fizz(n) + when {
+        n.rem(3) == 0 && n.rem(5) == 0 && n.rem(7) == 0 -> "BuzzWoof"
+        n.rem(3) == 0 && n.rem(7) == 0                  -> "Woof"
         n.rem(5) == 0 && n.rem(7) == 0                  -> "BuzzWoof"
-        n.rem(3) == 0 && n.rem(5) == 0                  -> "FizzBuzz"
-        n.rem(3) == 0                                   -> "Fizz"
+        n.rem(3) == 0 && n.rem(5) == 0                  -> "Buzz"
+        n.rem(3) == 0                                   -> ""
         n.rem(5) == 0                                   -> "Buzz"
         n.rem(7) == 0                                   -> "Woof"
         else                                            -> n.toString()
