465ee467b4133a58c48a2088e981e937d5956d20
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-05-30 10:31:21 +0100

    woof

--- src/test/kotlin/FizzBuzzWoofTests.kt
+++ src/test/kotlin/FizzBuzzWoofTests.kt
@@ -9,7 +9,7 @@ class FizzBuzzWoofTests {
         fizzBuzzWoof(4) shouldEqual "4"
         fizzBuzzWoof(5) shouldEqual "Buzz"
         fizzBuzzWoof(6) shouldEqual "Fizz"
-        fizzBuzzWoof(7) shouldEqual "7"
+        fizzBuzzWoof(7) shouldEqual "Woof"
         fizzBuzzWoof(8) shouldEqual "8"
         fizzBuzzWoof(9) shouldEqual "Fizz"
         fizzBuzzWoof(10) shouldEqual "Buzz"
@@ -21,5 +21,6 @@ fun fizzBuzzWoof(n: Int): String {
     if (n.rem(3) == 0 && n.rem(5) == 0) return "FizzBuzz"
     if (n.rem(3) == 0) return "Fizz"
     if (n.rem(5) == 0) return "Buzz"
+    if (n.rem(7) == 0) return "Woof"
     return n.toString()
 }
