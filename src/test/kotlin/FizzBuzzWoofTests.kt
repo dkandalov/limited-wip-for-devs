cf9eabb41405b87bbae319538814a91c0cc0a44b
dmitry.kandalov <dmitry.kandalov@gmail.com>
2020-06-11 15:07:06 +0100

    6

--- src/test/kotlin/FizzBuzzWoofTests.kt
+++ src/test/kotlin/FizzBuzzWoofTests.kt
@@ -8,12 +8,12 @@ class FizzBuzzWoofTests {
          fizzBuzzWoof(3) shouldEqual "Fizz"
          fizzBuzzWoof(4) shouldEqual "4"
          fizzBuzzWoof(5) shouldEqual "Buzz"
-         //fizzBuzzWoof(6) shouldEqual "Fizz"
+         fizzBuzzWoof(6) shouldEqual "Fizz"
     }
 }
 
 fun fizzBuzzWoof(n: Int): String {
+    if (n.rem(3) == 0) return "Fizz"
     if (n == 5) return "Buzz"
-    if (n == 3) return "Fizz"
     return n.toString()
 }
