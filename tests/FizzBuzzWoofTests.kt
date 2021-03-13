00fc68bd5809e646f5582e3fe6bf60475ffc1778
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-03-13 12:13:25 +0000

    ...

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -25,9 +25,9 @@ class FizzBuzzWoofTests {
 
 fun fizzBuzzWoof(n: Int): String {
     if (n.rem(3) == 0 && n.rem(5) == 0 && n.rem(7) == 0) return "FizzBuzzWoof"
-    if (n.rem(3) == 0 && n.rem(7) == 0) return "FizzWoof"
     if (n.rem(5) == 0 && n.rem(7) == 0) return "BuzzWoof"
     if (n.rem(3) == 0 && n.rem(5) == 0) return "FizzBuzz"
+    if (n.rem(3) == 0 && n.rem(7) == 0) return "FizzWoof"
     if (n.rem(3) == 0) return "Fizz"
     if (n.rem(5) == 0) return "Buzz"
     if (n.rem(7) == 0) return "Woof"
