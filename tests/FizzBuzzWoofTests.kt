83851a28b5e5d89c119e450d53688f2e5a75714b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-03-13 11:55:01 +0000

    refactoring

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -26,14 +26,9 @@ fun fizz(n: Int) = if (n.rem(3) == 0) "Fizz" else ""
 fun buzz(n: Int) = if (n.rem(5) == 0) "Buzz" else ""
 fun woof(n: Int) = if (n.rem(7) == 0) "Woof" else ""
 
-fun fizzBuzzWoof(n: Int): String {
-    return fizz(n) + buzz(n) + woof(n) + when {
-        n.rem(3) == 0                                   -> ""
-        n.rem(5) == 0                                   -> ""
-        n.rem(7) == 0                                   -> ""
-        else                                            -> n.toString()
-    }
-}
+fun fizzBuzzWoof(n: Int) =
+    (fizz(n) + buzz(n) + woof(n))
+        .ifEmpty { n.toString() }
 
 
 
