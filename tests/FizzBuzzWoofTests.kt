018970cf309c9490fbc340e292e6e303ba11b396
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-06-03 20:30:56 +0100

    tcr

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -1,15 +1,16 @@
+
 import datsok.shouldEqual
-import datsok.shouldNotEqual
 import org.junit.jupiter.api.Test
 
 class FizzBuzzWoofTests {
     @Test fun `convert number to FizzBuzzWoof string`() {
         fizzBuzzWoof(1) shouldEqual "1"
         fizzBuzzWoof(2) shouldEqual "2"
-        fizzBuzzWoof(3) shouldNotEqual "Fizz"
+        fizzBuzzWoof(3) shouldEqual "Fizz"
     }
 
     private fun fizzBuzzWoof(n: Int): String {
+        if (n == 3) return "Fizz"
         return n.toString()
     }
 }
