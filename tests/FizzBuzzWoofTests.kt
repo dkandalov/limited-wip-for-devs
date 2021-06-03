c21a27cc2796787b9be77035cedd2bc44705f1b6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-06-03 20:30:04 +0100

    tcr

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -5,9 +5,11 @@ class FizzBuzzWoofTests {
     @Test fun `convert number to FizzBuzzWoof string`() {
         fizzBuzzWoof(1) shouldEqual "1"
         fizzBuzzWoof(2) shouldEqual "2"
+        fizzBuzzWoof(3) shouldEqual "Fizz"
     }
 
     private fun fizzBuzzWoof(n: Int): String {
+        if (n == 3) return "Fizz"
         return n.toString()
     }
 }
