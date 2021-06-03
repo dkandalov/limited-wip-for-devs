6858d6fb98c1a67674790f40b5aeafb8cf5d40a6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-06-03 20:29:34 +0100

    tcr

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -1,8 +1,13 @@
+import datsok.shouldEqual
 import org.junit.jupiter.api.Test
 
 class FizzBuzzWoofTests {
     @Test fun `convert number to FizzBuzzWoof string`() {
-        // ...
+        fizzBuzzWoof(1) shouldEqual "1"
+    }
+
+    private fun fizzBuzzWoof(n: Int): String {
+        return "1"
     }
 }
 
