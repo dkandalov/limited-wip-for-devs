18ce2dcf6fcd3bf2b2e48833bc8ed633c1f52dc4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-06-25 10:15:59 +0100

    woof

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -28,14 +28,10 @@ fun woof(n: Int) = if (n.rem(7) == 0) "Woof" else ""
 
 fun fizzBuzzWoof(n: Int): String =
     fizz(n) + buzz(n) + woof(n) + when {
-        n.rem(3) == 0 && n.rem(5) == 0 && n.rem(7) == 0 -> ""
-        n.rem(3) == 0 && n.rem(7) == 0                  -> ""
-        n.rem(5) == 0 && n.rem(7) == 0                  -> ""
-        n.rem(3) == 0 && n.rem(5) == 0                  -> ""
-        n.rem(3) == 0                                   -> ""
-        n.rem(5) == 0                                   -> ""
-        n.rem(7) == 0                                   -> ""
-        else                                            -> n.toString()
+        n.rem(3) == 0 -> ""
+        n.rem(5) == 0 -> ""
+        n.rem(7) == 0 -> ""
+        else          -> n.toString()
     }
 
 
