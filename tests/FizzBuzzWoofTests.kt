d3ee890a4ca4028dbc252f141567259d70f4757d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-06-25 10:20:49 +0100

    tcr

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test
 class FizzBuzzWoofTests {
     @Test fun `convert number to FizzBuzzWoof string`() {
         fizzBuzzWoof(1) shouldEqual "1"
+        fizzBuzzWoof(2) shouldEqual "2"
     }
 
     private fun fizzBuzzWoof(n: Int): String {
-        return "1"
+        return n.toString()
     }
 }
 
