af7241c65b77f2cccadf76715ba8771f80396f39
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-06-25 10:10:02 +0100

    7,8,Fizz,Buzz,11

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -9,11 +9,22 @@ class FizzBuzzWoofTests {
         fizzBuzzWoof(4) shouldEqual "4"
         fizzBuzzWoof(5) shouldEqual "Buzz"
         fizzBuzzWoof(6) shouldEqual "Fizz"
+        fizzBuzzWoof(7) shouldEqual "7"
+        fizzBuzzWoof(8) shouldEqual "8"
+        fizzBuzzWoof(9) shouldEqual "Fizz"
+        fizzBuzzWoof(10) shouldEqual "Buzz"
+        fizzBuzzWoof(11) shouldEqual "11"
+        // fizzBuzzWoof(15) shouldEqual "FizzBuzz"
     }
 
     private fun fizzBuzzWoof(n: Int): String {
         if (n.rem(3) == 0) return "Fizz"
-        if (n == 5) return "Buzz"
+        if (n.rem(5) == 0) return "Buzz"
         return n.toString()
     }
 }
+
+
+
+
+
