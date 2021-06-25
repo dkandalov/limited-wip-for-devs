841382b1bf3d6f441ee153e1480e2e43c4fc0808
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-06-25 10:13:51 +0100

    woof

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -20,18 +20,19 @@ class FizzBuzzWoofTests {
         fizzBuzzWoof(5 * 7) shouldEqual "BuzzWoof"
         fizzBuzzWoof(3 * 5 * 7) shouldEqual "FizzBuzzWoof"
     }
-
-    private fun fizzBuzzWoof(n: Int): String {
-        if (n.rem(3) == 0 && n.rem(5) == 0 && n.rem(7) == 0) return "FizzBuzzWoof"
-        if (n.rem(3) == 0 && n.rem(7) == 0) return "FizzWoof"
-        if (n.rem(5) == 0 && n.rem(7) == 0) return "BuzzWoof"
-        if (n.rem(3) == 0 && n.rem(5) == 0) return "FizzBuzz"
-        if (n.rem(3) == 0) return "Fizz"
-        if (n.rem(5) == 0) return "Buzz"
-        if (n.rem(7) == 0) return "Woof"
-        return n.toString()
-    }
 }
+
+fun fizzBuzzWoof(n: Int): String =
+    when {
+        n.rem(3) == 0 && n.rem(5) == 0 && n.rem(7) == 0 -> "FizzBuzzWoof"
+        n.rem(3) == 0 && n.rem(7) == 0                  -> "FizzWoof"
+        n.rem(5) == 0 && n.rem(7) == 0                  -> "BuzzWoof"
+        n.rem(3) == 0 && n.rem(5) == 0                  -> "FizzBuzz"
+        n.rem(3) == 0                                   -> "Fizz"
+        n.rem(5) == 0                                   -> "Buzz"
+        n.rem(7) == 0                                   -> "Woof"
+        else                                            -> n.toString()
+    }
 
 
 
