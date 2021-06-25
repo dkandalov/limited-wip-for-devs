f768670e0897e7a954de3d728b4780fde0016490
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-06-25 10:24:01 +0100

    tcr

--- tests/FizzBuzzWoofTests.kt
+++ tests/FizzBuzzWoofTests.kt
@@ -20,17 +20,17 @@ class FizzBuzzWoofTests {
         fizzBuzzWoof(5 * 7) shouldEqual "BuzzWoof"
         fizzBuzzWoof(3 * 5 * 7) shouldEqual "FizzBuzzWoof"
     }
+}
 
-    private fun fizzBuzzWoof(n: Int): String {
-        if (n.rem(3) == 0 && n.rem(5) == 0 && n.rem(7) == 0) return "FizzBuzzWoof"
-        if (n.rem(5) == 0 && n.rem(7) == 0) return "BuzzWoof"
-        if (n.rem(3) == 0 && n.rem(7) == 0) return "FizzWoof"
-        if (n.rem(3) == 0 && n.rem(5) == 0) return "FizzBuzz"
-        if (n.rem(3) == 0) return "Fizz"
-        if (n.rem(5) == 0) return "Buzz"
-        if (n.rem(7) == 0) return "Woof"
-        return n.toString()
-    }
+fun fizzBuzzWoof(n: Int): String {
+    if (n.rem(3) == 0 && n.rem(5) == 0 && n.rem(7) == 0) return "FizzBuzzWoof"
+    if (n.rem(5) == 0 && n.rem(7) == 0) return "BuzzWoof"
+    if (n.rem(3) == 0 && n.rem(7) == 0) return "FizzWoof"
+    if (n.rem(3) == 0 && n.rem(5) == 0) return "FizzBuzz"
+    if (n.rem(3) == 0) return "Fizz"
+    if (n.rem(5) == 0) return "Buzz"
+    if (n.rem(7) == 0) return "Woof"
+    return n.toString()
 }
 
 
